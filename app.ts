baa14d5f5960363a7be6829b4e464b0ced852ecd
uuronn <uuronprogram@gmail.com>
2022-11-02 13:36:37 +0900

    Update: app.ts TS

--- app.ts
+++ app.ts
@@ -1,8 +1,19 @@
-const express = require("express");
-const app = express();
+import express, { Application, Request, Response } from "express";
 
-app.get("/", function (req: any, res: any) {
-  res.render("./pages/index.ejs", { title: "index title" });
+const app: Application = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req: Request, res: Response) {
+  res.send("test");
 });
 
-console.log("http://localhost:" + app.listen(3000).address().port);
+app.get("/sample", function (req: Request, res: Response) {
+  res.send("sample");
+});
+
+app.listen(PORT, () => {
+  console.log(`dev server running at: http://localhost:${PORT}/`);
+});
